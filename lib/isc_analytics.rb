3c225fbdd85c5baf2d787350e7ca8c3c4f41d10a
yon <Lighthawky@gmail.com>
2012-12-24 09:50:32 +0200

    added bootstrap

--- lib/isc_analytics.rb
+++ lib/isc_analytics.rb
@@ -2,6 +2,7 @@ require "#{File.dirname(__FILE__)}/isc_analytics/engine"
 require "#{File.dirname(__FILE__)}/isc_analytics/config"
 require "#{File.dirname(__FILE__)}/isc_analytics/client_api"
 require "#{File.dirname(__FILE__)}/isc_analytics/exceptions"
+require "#{File.dirname(__FILE__)}/isc_analytics/bootstrap"
 require "#{File.dirname(__FILE__)}/isc_analytics/controller_support"
 
 module IscAnalytics
